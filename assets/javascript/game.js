349b6aa1b4ffb67d0ea32cff060ed34cbbd90f7a
Yev Ro <yev.rokhin@gmail.com>
2020-04-19 10:53:06 -0700

    Continuing to add possible script that I need and I try to process this game's logic - I think I might have bit off more than I can chew :P.  Updated html id to try to reflect the needs in java and vice versa

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,12 +7,9 @@
 //6. End Conditions (win/los)
 //7. Display the number of wins
 
-
-
 //Variables
 var words = ["Roxalani", "Alani", "Alans", "Scythians", "Romans", "Macedonians", "Athenians", "Spartans", "Carthigians", "Sarmatians", "Dacians"];
 var word = words[Math.floor(Math.random() * words.length)];
-var remainingLetters = word.length;
 
 //var of to start game with 0 wins
 var wins = 0;
@@ -20,42 +17,66 @@ var wins = 0;
 //var to hold the index of current word
 var wordIndex = 0;
 
+//remaining guess var
+var
+
 //Vars w/ references
-var userChoiceText = document.getElementById("user-guess");
-var secretWordText = document.getElementById("secret-word");
-var remainingGuessText = document.getElementById("remaining-guesses");
-var winsText = document.getElementById("wins");
+var wordSelectedElement = document.getElementById("selected-word");
+var winsElement = document.getElementById("wins");
+var userGuessElement = document.getElementById("user-guess");
+var guessedLettersElement = document.getElementById("guessed-letters")
+var remainingGuessElement = document.getElementById("remaining-guesses");
+
 
 //need to identify what user can choose - letter wise and add in toLowerCase().charAt()
+var validGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Loop
+//Start Game, Reset Game, Win, Lose Variables
+var pressAnyKeyToStart = ["Press Any Key to Start"];
+var pressAnyKeyToReset = ["Press Any Key to Reset"];
+var youWin = ["You Win"];
+var youLose = ["You Lose"];
+
+//Loop/Answer Array
+var answerArray = [];
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
-
-    var answerArray = [];
 }
+
+var remainingLetters = word.length;
+
+//Updating the Answer Array
+for (var j = 0; j < word.length; j++) {
+    if (word[j] === userGuess) {
+        answerArray[j] = userGuess; remainingLetters--;
+    }
+};
 
 //Rendering the word function
 function renderWord() {
     if (wordInedx <= (words.lenght - 1)) {
-        document.querySelector("#secret-word").innerHTML = words[wordIndex];
+        document.querySelector("#selected-word").innerHTML = words[wordIndex].answerArray;
     }
     else {
-        document.querySelector("#secret-word").innerHTML = "There are no more words to guess!";
+        document.querySelector("#selected-word").innerHTML = "There are no more words to guess!";
         document.querySelector("#wins").innerHTML = "Wins: " + wins;
     }
-}
+};
 
 //Function to update the wins.
 function updateWins() {
     document.querySelector("#wins").innerHTML = "Wins: " + wins;
-}
+};
 
 //MAIN GAME START
 
 //Starting game functions
 renderWord();
 updateWins();
+
+//game variable
+
+var game = Hangman();
 
 //function for onkeyup to start game
 document.onkeyup = function (event) {
@@ -66,9 +87,28 @@ document.onkeyup = function (event) {
     }
 
     //Determines what  which key was pressed and makes it lowercase
-    var userInput = event.key.toLowerCase();
+    var userGuess = event.key.toLowerCase();
+};
+
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+
+    if (validGuess.includes(userGuess) && !game.guessedLetters) {
+        game.checkGuess(userGuess);
+    }
+} else {
+    game.updatePageData();
 }
 
+//correct guess
+
+
+//incorrect guess
+
+//update game state
+var updateGameState = function (guess, word, answerArray) {
+
+};
 
 //function for letter changing from "_ _ _ _ _" to "A _ a _ _ _"
 
