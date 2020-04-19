9fddb3cc65d7eb404656f6b126b8da96e5637a75
Yev Ro <yev.rokhin@gmail.com>
2020-04-19 07:45:57 -0700

    Added some functions in regards to starting game and additional variables.  Also modified HTML to try to match functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,29 +14,66 @@ var words = ["Roxalani", "Alani", "Alans", "Scythians", "Romans", "Macedonians",
 var word = words[Math.floor(Math.random() * words.length)];
 var remainingLetters = word.length;
 
+//var of to start game with 0 wins
 var wins = 0;
+
+//var to hold the index of current word
+var wordIndex = 0;
 
 //Vars w/ references
 var userChoiceText = document.getElementById("user-guess");
-var letterGuessText = document.getElementById("letter-guessed");
+var secretWordText = document.getElementById("secret-word");
 var remainingGuessText = document.getElementById("remaining-guesses");
 var winsText = document.getElementById("wins");
 
-
-var answerArray = [];
+//need to identify what user can choose - letter wise and add in toLowerCase().charAt()
 
 //Loop
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
+
+    var answerArray = [];
 }
 
-//function for onkeyup
+//Rendering the word function
+function renderWord() {
+    if (wordInedx <= (words.lenght - 1)) {
+        document.querySelector("#secret-word").innerHTML = words[wordIndex];
+    }
+    else {
+        document.querySelector("#secret-word").innerHTML = "There are no more words to guess!";
+        document.querySelector("#wins").innerHTML = "Wins: " + wins;
+    }
+}
 
+//Function to update the wins.
+function updateWins() {
+    document.querySelector("#wins").innerHTML = "Wins: " + wins;
+}
+
+//MAIN GAME START
+
+//Starting game functions
+renderWord();
+updateWins();
+
+//function for onkeyup to start game
 document.onkeyup = function (event) {
-    var userGuesss = event.key;
-    //need to write code on what happens once user presses a key
 
-    //display what letter was guessed and storing letters and wins
-    userChoiceText.textContent = userGuess;
-    winsText = textContent = "Wins: " + wins;
-};
+    //stop function
+    if (wordIndex === words.length) {
+        return;
+    }
+
+    //Determines what  which key was pressed and makes it lowercase
+    var userInput = event.key.toLowerCase();
+}
+
+
+//need to write code on what happens once user presses a key
+
+
+//display what letter was guessed and storing letters and wins
+userChoiceText.textContent = ;
+winsText = textContent = "Wins: " + wins;
+;
