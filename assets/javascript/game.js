3e1eb0299b2424148f516b656629127ba6879716
Yev Ro <yev.rokhin@gmail.com>
2020-04-25 09:08:05 -0700

    working on the render word function and replace word with '_'.  Also update styles to center divs and make the image fit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,53 @@
 //1.  Random world list (minor)
 var words = ["Roxalani", "Alani", "Alans", "Scythians", "Romans", "Macedonians", "Athenians", "Spartans", "Carthigians", "Sarmatians", "Dacians"];
 //2.  Random word select (minor)
-var word = words[Math.floor(Math.random() * words.length)];
+var word = words[Math.floor(Math.random() * 10)];
+//2a.  Setting variable for word index so that we can render a word on the page.
+var wordIndex = 0;
+//2b. Letters in a word variable
+var remainLetters = word.length;
+//2c. Wins variable
+var wins = 0;
+//2d.  Valid letter guesses
+var validGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var answerArray = ["_"];
 
 
 //3.  SECTION 1:  GETTING THE WORD  IN "_" BLANK SPACES
-var remainLetters = word.length;
-//3a. Generate blank spaces instead of word
+//3b. Generate a word
+function renderWord() {
+    document.querySelector("#selected-word").innerHTML = "Selected Word: " + word;
+    //3b1.  letters in word changed into underscore "_"
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_"
+    };
+    console.log(remainLetters)
+};
 
 
+
+function renderWins() {
+    document.querySelector("#wins").innerHTML = "Wins: " + wins;
+};
+
+
+//3c. Replace the letters in a word with "_" spaces
+
+//3d. Calling the functions
+renderWord()
+renderWins()
 
 //4.  SECTION 2:  USER LETTER GUESS
 //4a.  Start Function (onkeyup)
+document.onkeyup = function (event) {
+    document.querySelector("#selected-word").innerHTML = "_" + word;
+    var userGuess = event.key.toLowerCase();
+    for (var j = 0; i < validGuess.length; i++) {
+        answerArray[j]
+    };
+};
+
+console.log(answerArray)
 //4b.  Valid guesses
 
 
@@ -26,7 +62,7 @@ var remainLetters = word.length;
 
 //6.  SECTION 4:  WIN/LOSE CONDITIONS
 //6a. If all letters guessed, else: win - else if: lose.
-//6b. Upgrade the wins counter
+//6b. Update the wins counter (scoreboard)
 
 
 
