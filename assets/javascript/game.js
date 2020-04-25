d9830e28921dede8f26729f957169e7a73fe5cc5
Yev Ro <yev.rokhin@gmail.com>
2020-04-24 17:36:36 -0700

    Restarting pseudo code.  Modified html to add image.  Image added to file - will style image later.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,111 +1,33 @@
-//Sections/Steps of codes for Game
-//1. Random Words
-//2. Word Selection
-//3. Key functions (on keyup.etc)
-//4. Guessing the letter/current guess and guess remaining
-//5. Letters already guessed
-//6. End Conditions (win/los)
-//7. Display the number of wins
+//PSUDOCODE AND CODE FOR HANGMAN GAME:
 
-//Variables
+
+//1.  Random world list (minor)
 var words = ["Roxalani", "Alani", "Alans", "Scythians", "Romans", "Macedonians", "Athenians", "Spartans", "Carthigians", "Sarmatians", "Dacians"];
+//2.  Random word select (minor)
 var word = words[Math.floor(Math.random() * words.length)];
 
-//var of to start game with 0 wins
-var wins = 0;
 
-//var to hold the index of current word
-var wordIndex = 0;
-
-//remaining guess var
-var
-
-//Vars w/ references
-var wordSelectedElement = document.getElementById("selected-word");
-var winsElement = document.getElementById("wins");
-var userGuessElement = document.getElementById("user-guess");
-var guessedLettersElement = document.getElementById("guessed-letters")
-var remainingGuessElement = document.getElementById("remaining-guesses");
+//3.  SECTION 1:  GETTING THE WORD  IN "_" BLANK SPACES
+var remainLetters = word.length;
+//3a. Generate blank spaces instead of word
 
 
-//need to identify what user can choose - letter wise and add in toLowerCase().charAt()
-var validGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Start Game, Reset Game, Win, Lose Variables
-var pressAnyKeyToStart = ["Press Any Key to Start"];
-var pressAnyKeyToReset = ["Press Any Key to Reset"];
-var youWin = ["You Win"];
-var youLose = ["You Lose"];
-
-//Loop/Answer Array
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-
-var remainingLetters = word.length;
-
-//Updating the Answer Array
-for (var j = 0; j < word.length; j++) {
-    if (word[j] === userGuess) {
-        answerArray[j] = userGuess; remainingLetters--;
-    }
-};
-
-//Rendering the word function
-function renderWord() {
-    if (wordInedx <= (words.lenght - 1)) {
-        document.querySelector("#selected-word").innerHTML = words[wordIndex].answerArray;
-    }
-    else {
-        document.querySelector("#selected-word").innerHTML = "There are no more words to guess!";
-        document.querySelector("#wins").innerHTML = "Wins: " + wins;
-    }
-};
-
-//Function to update the wins.
-function updateWins() {
-    document.querySelector("#wins").innerHTML = "Wins: " + wins;
-};
-
-//MAIN GAME START
-
-//Starting game functions
-renderWord();
-updateWins();
-
-//game variable
-
-var game = Hangman();
-
-//function for onkeyup to start game
-document.onkeyup = function (event) {
-    var userGuess = event.key.toLowerCase();
-
-    if (validGuess.includes(userGuess) && !game.guessedLetters) {
-        game.checkGuess(userGuess);
-    }
-} else {
-    game.updatePageData();
-
-    //Determines what  which key was pressed and makes it lowercase
-    var userGuess = event.key.toLowerCase();
-};
-
-//correct guess
+//4.  SECTION 2:  USER LETTER GUESS
+//4a.  Start Function (onkeyup)
+//4b.  Valid guesses
 
 
-//incorrect guess
-
-//update game state
-var updateGameState = function (guess, word, answerArray) {
-
-};
-
-//function for letter changing from "_ _ _ _ _" to "A _ a _ _ _"
+//5.  SECTION 3:  SCRIPT REACT TO USER GUESS
+//5a. CORRECT GUESS, then change the "_" into a letter correctly guessed.
+//5b. INCORRECT GUESS, then add letter to already to list below 
+//5c. INCORRECT GUESS, removes guesses reamining #.
 
 
-//display what letter was guessed and storing letters and wins
-userChoiceText.textContent = ;
-winsText = textContent = "Wins: " + wins;
-;
+//6.  SECTION 4:  WIN/LOSE CONDITIONS
+//6a. If all letters guessed, else: win - else if: lose.
+//6b. Upgrade the wins counter
+
+
+
+
