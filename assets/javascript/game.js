d41c8069332ddedb94c0098839bd37d4c5f631b7
Yev Ro <yev.rokhin@gmail.com>
2020-09-23 08:51:14 -0700

    added some functionality, certain elements display - need to fix word coutner and letter guessed replacement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,119 @@
 //PSUDOCODE AND CODE FOR HANGMAN GAME:
 
-
 //1.  Random world list (minor)
-var words = ["Roxalani", "Alani", "Alans", "Scythians", "Romans", "Macedonians", "Athenians", "Spartans", "Carthigians", "Sarmatians", "Dacians"];
+var words = ["ROXALANI", "ALANI", "ALANS", "SCYTHIANS", "ROMANS", "MACEDONIANS", "ATHENIANS", "SPARTANS", "CARTHAGIANS", "SARMATIANS", "DACIANS", "BOII", "ARVERNI", "PARTHIANS", "SELEUCID", "BAKTRIA", "LUSITANI", "EPIRUS", "MASSAGETAE", "PONTUS", "PERGAMON", "CIMMERIA", "ICENI", "SUEBI"];
 //2.  Random word select (minor)
-var word = words[Math.floor(Math.random() * 10)];
+var word = words[Math.floor(Math.random() * (words.length))];
+
 //2a.  Setting variable for word index so that we can render a word on the page.
 var wordIndex = 0;
 //2b. Letters in a word variable
 var remainLetters = word.length;
-//2c. Wins variable
-var wins = 0;
 //2d.  Valid letter guesses
 var validGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var answerArray = [];
 var dashStore = [];
-var remainGuess = 11;
-var remainGuessLtr = [];
+var maxTries = 10;
+var guessedLetters = [];
 var dashArray = [];
+var gameStarted = false;
+var gameFinished = false;
+//2c. Wins variable
+var wins = 0;
+
+var selectedWord = document.querySelector("#selected-word").innerHTML;
 
 
-//3.  SECTION 1:  GETTING THE WORD  IN "_" BLANK SPACES
-//3b. Generate a word
+//SECTION 1:  GETTING THE WORD  IN "_" BLANK SPACES
+
+//Game Start - Rendering a random word
+function gameStart() {
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = " _ ";
+    };
+}
+
+//Generating/Rendering a Word
 function renderWord() {
     document.querySelector("#selected-word").innerHTML = "Selected Word: " + word;
     //3b1. Target variable and replace the letters in the variable into underscore "_"
-    for (var i = 0; i < word.length; i++) {
-        answerArray[i] = " _ ";
-        // dashStore = dashStore + answerArray[i];
-    };
-    // document.querySelector("#selected-word").innerHTML = dashStore;
 
-    console.log(word)
+    let testArray = "";
+    for (var i = 0; i < answerArray.length; i++) {
+        testArray = testArray + answerArray[i];
+    }
+
+    document.querySelector("#selected-word").innerHTML = testArray;
+
 };
 
+//Generating/Rendering Wins
 function renderWins() {
     document.querySelector("#wins").innerHTML = "Wins: " + wins;
 };
 
-function renderDash() {
-    for (var i = 0; i < word.length; i++) {
-        dashArray[i] = " _ ";
-        dashStore = dashStore + answerArray[i];
-        document.querySelector("#selected-word").innerHTML = dashStore;
-    };
+//Generating/Rendering Remaining-Guess
+function renderMaxTries() {
+    document.querySelector("#remaining-guesses").innerHTML = "Guesses Left: " + maxTries;
 };
 
-//3c. Replace the letters in a word with "_" spaces
+//Generating/
+function renderGuessedLetters() {
+    document.querySelector("#guessed-letters").innerHTML = "Letters Already Guessed: " + guessedLetters;
 
-//3d. Calling the functions
+    // document.querySelector("#guessed-letters").innerHTML = guess;
+};
+
+console.log(word)
+
+//Reset game button and function --------- NEED TO BUILD
+
+//Calling the functions (function needs to be prioritized as to what is called first)
+gameStart()
 renderWord()
 renderWins()
-renderDash()
+renderMaxTries()
+renderGuessedLetters();
+
 
 //4.  SECTION 2:  USER LETTER GUESS
 //4a.  Start Function (onkeyup)
 document.onkeyup = function (event) {
-    var userGuess = event.key.toLowerCase();
+    var userGuess = event.key.toUpperCase();
+
+    if (wordIndex === word.length) {
+        return;
+    }
+
     for (var i = 0; i < word.length; i++) {
-        if (userGuess == word[i]) {
-            dashStore[i] = word[i];
-            console.log("match")
+        if (userGuess === word[i]) {
+            answerArray[i] = word[i];
+
+        } else {
+            renderGuessedLetters();
+            maxTries -= 1;
+            renderMaxTries()
+            if
+                (maxTries === 0) {
+                // alert("You Lose!");
+            }
         }
+
         console.log(userGuess)
-        console.log(word[i])
-        // else {
         //     remainGuess--;
 
         //     remainGuessLtr = remainGuessLtr + " " + userGuess;
         //     document.querySelector("#guessed-letters").innerHTML = "Letters Already Guessed: " + remainGuessLtr;
         // }
     };
-    document.querySelector("#selected-word").innerHTML = dashStore;
+
+    renderWord();
+    renderGuessedLetters();
+
+
 };
 
 console.log(answerArray)
-//4b.  Valid guesses
-
-
-//5.  SECTION 3:  SCRIPT REACT TO USER GUESS
-//5a. CORRECT GUESS, then change the "_" into a letter correctly guessed.
-//5b. INCORRECT GUESS, then add letter to already to list below 
-//5c. INCORRECT GUESS, removes guesses reamining #.
-
-
-//6.  SECTION 4:  WIN/LOSE CONDITIONS
-//6a. If all letters guessed, else: win - else if: lose.
-//6b. Update the wins counter (scoreboard)
 
 
 
